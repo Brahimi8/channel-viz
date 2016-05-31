703a912c2b0f2be72bd496c2ee5e9433123dbad6
BENKAHLA Miloud <ibrahim_mld@hotmail.fr>
2016-05-31 16:35:14 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 	var defaultKey		= 'IYkBJ2XowrASAD0LJuB1uv1pYEGbaSEn189Gr1dsQcisI3rm', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [457250874], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Visualisation de Telosb', // Replaces Xively logo in the header
-		dataDuration	= '5days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '1FA055', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
