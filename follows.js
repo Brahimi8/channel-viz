7b952139fbfbb6174a577d6c0c4083c4dfe19bf6
Brahimi8 <ibrahim_mld@hotmail.fr>
2016-05-31 12:03:36 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'IYkBJ2XowrASAD0LJuB1uv1pYEGbaSEn189Gr1dsQcisI3rm', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [457250874], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'aaaa', // Replaces Xively logo in the header
+		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
